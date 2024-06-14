eb4a0f839e86f7e5c8f562fbe06b4671b18f8b8d
sarthakkurothe <reachsarthakkurothe@gmail.com>
2024-06-14 18:15:39 +0530

    initial commit

---
+++ SlidelyAITask/SlidelyAITask/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+    Public Sub New()
+        InitializeComponent()
+        MsgBox("Sarthak Kurothe here, this is my Slidely AI task")
+    End Sub
+
+End Class
---
+++ SlidelyAITask/SlidelyAITask/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("SlidelyAITask")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP")>
+<Assembly: AssemblyProduct("SlidelyAITask")>
+<Assembly: AssemblyCopyright("Copyright © HP 2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("7fdea1d2-3af6-497b-815c-7877f13f485f")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
